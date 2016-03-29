f1f3236afb2b1e5692a1c7a871807af4c1e14ca4
hookssi <sounghoo12@gmail.com>
2016-03-29 20:32:17 +0900

    적 비행체의 구현과 종류 정리

--- Bit_Blaster/Assets/Script/FlightObject_Script.cs
+++ Bit_Blaster/Assets/Script/FlightObject_Script.cs
@@ -5,15 +5,12 @@ public class FlightObject_Script : MonoBehaviour {
 
     /* 이동 관련 변수 */
     protected Rigidbody2D m_Rigid; // rigidbody
-    protected Vector2 m_Direction; // 방향
-    protected float m_MaxVelocity; // 최대 속도
-    protected float m_ForceScale; // 힘크기
+    private Vector2 m_Direction = Vector2.down; // 방향
+    private float m_MaxVelocity = 1f; // 최대 속도
+    private float m_ForceScale = 1f; // 힘크기
 
     void Awake ()
     {
-        m_MaxVelocity = 1f; 
-        m_Direction = Vector2.down; 
-        m_ForceScale = 1f; 
         m_Rigid = GetComponent<Rigidbody2D>();
     }
 	
@@ -22,7 +19,7 @@ public class FlightObject_Script : MonoBehaviour {
         Move();
     }
 
-    void Move()
+    protected virtual void Move()
     {
         m_Rigid.AddForce(m_Direction * m_ForceScale,ForceMode2D.Impulse);
 
@@ -33,8 +30,23 @@ public class FlightObject_Script : MonoBehaviour {
             m_Rigid.velocity = new Vector2(m_Rigid.velocity.x, NormalizeValue(m_Rigid.velocity.y) * m_MaxVelocity);
     }
 
-    int NormalizeValue(float a)
+    protected int NormalizeValue(float a)
     {
         return (int)(a / Mathf.Abs(a));
+    }
+
+    public void SetDirection(Vector2 p_Direction)
+    {
+        m_Direction = p_Direction;
+    }
+
+    public void SetMaxVelocity(float p_MaxVelocity)
+    {
+        m_MaxVelocity = p_MaxVelocity;
+    }
+
+    public void SetForceScale(float p_ForceScale)
+    {
+        m_ForceScale = p_ForceScale;
     }
 }
