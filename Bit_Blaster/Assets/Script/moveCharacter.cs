de2870e0be97422cb036656aa52a8188330950db
nan-anya <rjsxkr1122@naver.com>
2016-03-29 21:05:04 +0900

    주인공 우주선 만듬 2.

---
+++ Bit_Blaster/Assets/Script/moveCharacter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCharacter : MonoBehaviour
+{
+
+    public float characterSpeed = 2;
+
+    void Start ()
+    {
+	    
+	}
+	
+
+	void Update ()
+    {
+        move();
+
+    }
+
+    void move()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+
+        float dirY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(dirX, dirY);
+
+        GetComponent<Rigidbody2D>().velocity = movement * characterSpeed;
+
+    }
+
+}
