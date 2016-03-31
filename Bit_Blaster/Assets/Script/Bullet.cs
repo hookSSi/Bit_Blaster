19fd22358e1ecebdea89cc41bc09f1dd2e6c415d
nan-anya <rjsxkr1122@naver.com>
2016-03-31 14:45:31 +0900

    ignore파일 수정 꼭받아주세요 -성욱

---
+++ Bit_Blaster/Assets/Script/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : FlightObject_Script
+{
+    public float m_AttackSpeed;
+
+    public float m_BulletSpeed;
+
+    public float m_BulletMaintainTime;
+
+    public float m_Damage;
+
+	void Update ()
+    {
+	
+	}
+
+    void Move()
+    {
+
+
+    }
+
+}
