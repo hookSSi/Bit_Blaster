ac2adf87466e545c631944f6622b0b2dd3a52032
nan-anya <rjsxkr1122@naver.com>
2016-04-02 17:09:10 +0900

    타이틀 & 게임오버 화면

---
+++ Bit_Blaster/Assets/Script/UI/ReStartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReStartGame : MonoBehaviour
+{
+
+    public void loadTitle()
+    {
+        Application.LoadLevel("title");
+    }
+
+}
---
+++ Bit_Blaster/Assets/Script/UI/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour
+{
+
+    public void loadGame()
+    {
+        Application.LoadLevel("1");
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Application.LoadLevel("1");
+        }
+    }
+
+}
