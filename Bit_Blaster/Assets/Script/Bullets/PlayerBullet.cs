83dfabd1b9636b2cc215c6b180457709bbd3abd5
zxc99as <zxc99as@naver.com>
2016-04-02 16:57:48 +0900

    총알 패턴 추가 아이템 유도 기능 추가 아이템 스트립트 추가

--- Bit_Blaster/Assets/Script/Bullets/PlayerBullet.cs
+++ Bit_Blaster/Assets/Script/Bullets/PlayerBullet.cs
@@ -4,45 +4,54 @@ using System.Collections;
 public class PlayerBullet : FlightObject_Script
 {
 	public float bulletAngle;
+	public float bulletDelay;
 
 	private float speed;
-	public float bulletDelay;
-	private Transform playerTransform;
+	
+
+	public GameObject target;
 
 	// Use this for initialization
 	void Start()
 	{
-		speed = 0.3f;
+		speed = 0.2f;
 		bulletDelay = 0.2f;
-		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
 		Move();
-		CheckPosition();
+		DestroyOutOfMap();
 	}
 
 	protected override void Move()
 	{
 		this.transform.position += new Vector3(-Mathf.Sin(bulletAngle * Mathf.Deg2Rad) * speed, Mathf.Cos(bulletAngle * Mathf.Deg2Rad) * speed, 0);
-	}
+		/*if (target.GetComponent<DetectRange>().sprite == null)
+			this.transform.position += new Vector3(-Mathf.Sin(bulletAngle * Mathf.Deg2Rad) * speed, Mathf.Cos(bulletAngle * Mathf.Deg2Rad) * speed, 0);
 
-	private void CheckPosition()
-	{
-		if (Mathf.Abs(playerTransform.position.x - this.transform.position.x) > 20
-			|| Mathf.Abs(playerTransform.position.y - this.transform.position.y) > 30)
+		else
 		{
-			Destroy(this.gameObject);
-		}
+			Transform tmp = target.GetComponent<DetectRange>().sprite.transform;
+
+			if (Mathf.Abs(Mathf.Atan2(tmp.TransformPoint(Vector3.zero).y - this.transform.position.y, tmp.TransformPoint(Vector3.zero).x - this.transform.position.x) * Mathf.Rad2Deg - this.transform.eulerAngles.z) < 22.5f)
+			{
+				bulletAngle = Mathf.Atan2(tmp.TransformPoint(Vector3.zero).y - this.transform.position.y, tmp.TransformPoint(Vector3.zero).x - this.transform.position.x) * Mathf.Rad2Deg;
+				this.transform.position += new Vector3(-Mathf.Sin(bulletAngle * Mathf.Deg2Rad) * speed, Mathf.Cos(bulletAngle * Mathf.Deg2Rad) * speed, 0);
+				Debug.Log(bulletAngle);
+			}
+
+			else
+			{
+				this.transform.position += new Vector3(-Mathf.Sin(bulletAngle * Mathf.Deg2Rad) * speed, Mathf.Cos(bulletAngle * Mathf.Deg2Rad) * speed, 0);
+				//Debug.Log(Mathf.Abs(Mathf.Atan2(target.transform.TransformPoint(Vector3.zero).y - this.transform.position.y, target.transform.TransformPoint(Vector3.zero).x - this.transform.position.x)) * Mathf.Rad2Deg);
+			}
+
+		}*/
+		//Debug.Log(Mathf.Atan2(target.transform.position.y - this.transform.position.y, target.transform.position.x - this.transform.position.x) * Mathf.Deg2Rad);
 	}
 
-	/*public void SetAngle(float nAngle)
-	{
-		this.bulletAngle = nAngle;
-	}
-	*/
 	public void SetEulerAngleZ(float nAngle)
 	{
 		this.bulletAngle = nAngle;
@@ -51,5 +60,17 @@ public class PlayerBullet : FlightObject_Script
 	public float GetBulletDelay()
 	{
 		return this.bulletDelay;
+	}
+
+	protected void SetTarget(GameObject nTarget)
+	{
+		//Debug.Log(nTarget);
+		this.target = nTarget;
+		//Debug.Log(target);
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Enemy") Destroy(gameObject);
 	}
 }
