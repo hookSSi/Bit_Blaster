e19abb50c7ac4f7e9a1274a4aa8bf8ac57df1755
zxc99as <zxc99as@naver.com>
2016-04-02 19:19:34 +0900

    적 총알 추가 적 너프 필요한듯

---
+++ Bit_Blaster/Assets/Script/Bullets/GrayEnemyBullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrayEnemyBullet : FlightObject_Script
+{
+
+	public float bulletAngle;
+	public float bulletDelay;
+
+	private float speed;
+
+	// Use this for initialization	
+	void Start()
+	{
+		speed = 0.2f;
+		bulletDelay = 5f;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		Move();
+		DestroyOutOfMap();
+	}
+
+	protected override void Move()
+	{
+		this.transform.position += new Vector3(-Mathf.Sin(bulletAngle * Mathf.Deg2Rad) * speed, Mathf.Cos(bulletAngle * Mathf.Deg2Rad) * speed, 0);
+	}
+
+	public void SetEulerAngleZ(float nAngle)
+	{
+		GameObject tmpPlayer = GameObject.FindWithTag("Player");
+		this.bulletAngle = nAngle;
+		bulletAngle = Mathf.Atan2(tmpPlayer.transform.position.y - this.transform.position.y, tmpPlayer.transform.position.x - this.transform.position.x) * Mathf.Rad2Deg;
+	}
+}
--- Bit_Blaster/Assets/Script/Enemy/EnemyGreen_script.cs
+++ Bit_Blaster/Assets/Script/Enemy/EnemyGreen_script.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class EnemyGreen_script : Enemy_script
 {
-    void Awake()
+	private FlightObject_Script temp;
+
+	void Awake()
     {
         m_Rigid = GetComponent<Rigidbody2D>();
         SetDirection(Vector2.up);
@@ -13,4 +15,10 @@ public class EnemyGreen_script : Enemy_script
         m_Score = 300;
         InvokeRepeating("FireBullet", 1f, m_FireRate);
     }
+
+	protected override void FireBullet() // 공격 처리
+	{
+		m_Bullet.GetComponent<GreenEnemyBullet>().SetEulerAngleZ(this.gameObject.transform.eulerAngles.z);
+		Instantiate(m_Bullet, m_FirePosition.position, Quaternion.Euler(new Vector3(0, 0, this.transform.eulerAngles.z)));
+	}
 }
