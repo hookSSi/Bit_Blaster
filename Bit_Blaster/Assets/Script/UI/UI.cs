f8a787bfa3bb04ba89643b0ce1541df85df7b88c
nan-anya <rjsxkr1122@naver.com>
2016-04-02 12:32:38 +0900

    점수 표시 됨.

--- Bit_Blaster/Assets/Script/UI/UI.cs
+++ Bit_Blaster/Assets/Script/UI/UI.cs
@@ -1,25 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 
 
 public class UI : MonoBehaviour
 {
 
-	void Start ()
+    bool m_TimeCheck = true;
+
+    float m_ScoreEarningPeriod = 2;
+
+    int Score = 0;
+
+    GameObject m_PointTaxt;
+
+    void Start ()
     {
-	
+        m_PointTaxt = GameObject.Find("Point");
 	}
 	
 	void Update ()
     {
-	
-	}
+        EarnScoreCheck();
+    }
 
-    void EarnScore()
+    void EarnScoreCheck()
     {
+        if (m_TimeCheck == true)
+        {
+            StartCoroutine("EarnScore");
+        }
 
+    }
 
+    IEnumerator EarnScore()
+    {
+        m_TimeCheck = false;
+
+        Score += 10;
+
+        m_PointTaxt.GetComponent<Text>().text = System.Convert.ToString(Score);
+
+        yield return new WaitForSeconds(m_ScoreEarningPeriod);
+
+        m_TimeCheck = true;
     }
 
 }
